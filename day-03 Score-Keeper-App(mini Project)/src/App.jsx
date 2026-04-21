7f76c038087e0a72542f3d85e1b95fc55dbf891e
Austin <austinsurya7@gmail.com>
2026-04-21 17:50:15 +0530

    Basic Setup done

--- day-03 Score-Keeper-App(mini Project)/src/App.jsx
+++ day-03 Score-Keeper-App(mini Project)/src/App.jsx
@@ -5,11 +5,26 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  
+    
+  let score = 0;
+  let wickets = 0;
 
   return (
     <>
+      <h1>Score-Keeper-App</h1>
+      <h2>Score : {score}/{wickets}</h2>
+
+      <div>
+         <button>1</button>
+         <button>2</button>
+         <button>3</button>
+         <button>4</button>
+         <button>5</button>
+         <button>6</button>
+         <button>WIcket</button>
+      </div>
       
+
     </>
   )
 }
