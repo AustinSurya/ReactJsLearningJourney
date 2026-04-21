c11a758ad77f31fa13749880946882f684248e42
Austin <austinsurya7@gmail.com>
2026-04-21 17:58:06 +0530

    Eventhandlers added

--- day-03 Score-Keeper-App(mini Project)/src/App.jsx
+++ day-03 Score-Keeper-App(mini Project)/src/App.jsx
@@ -9,19 +9,31 @@ function App() {
   let score = 0;
   let wickets = 0;
 
+  function addscore(num) {
+   score += num;
+  }
+  
+   function addwicket() {
+
+  if(wickets<10){
+    wickets +=1;
+  }
+  
+  }
+
   return (
     <>
       <h1>Score-Keeper-App</h1>
       <h2>Score : {score}/{wickets}</h2>
 
       <div>
-         <button>1</button>
-         <button>2</button>
-         <button>3</button>
-         <button>4</button>
-         <button>5</button>
-         <button>6</button>
-         <button>WIcket</button>
+         <button onClick={(num) => addscore(1)} >1</button>
+        <button onClick={(num) => addscore(2)} >2</button>
+        <button onClick={(num) => addscore(3)} >3</button>
+        <button onClick={(num) => addscore(4)} >4</button>
+        <button onClick={(num) => addscore(5)} >5</button>
+        <button onClick={(num) => addscore(6)} >6</button>
+        <button onClick={(num) => addscore(wicket)} >wicket</button>
       </div>
       
 
